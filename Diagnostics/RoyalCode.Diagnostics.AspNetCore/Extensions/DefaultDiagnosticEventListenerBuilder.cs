46e1ceb6a7789cf7816f427bd6c2adcc6dbaf988
eglauko <eglauko@hotmail.com>
2022-03-04 10:11:38 -0300

    copy the aspnetcore diagnostics code and review.

---
+++ Diagnostics/RoyalCode.Diagnostics.AspNetCore/Extensions/DefaultDiagnosticEventListenerBuilder.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Internal default implementation of <see cref="IDiagnosticEventListenerBuilder"/>;
+    /// </summary>
+    internal class DefaultDiagnosticEventListenerBuilder : IDiagnosticEventListenerBuilder
+    {
+        public IServiceCollection Services { get; }
+
+        internal DefaultDiagnosticEventListenerBuilder(IServiceCollection services)
+        {
+            Services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+    }
+}
