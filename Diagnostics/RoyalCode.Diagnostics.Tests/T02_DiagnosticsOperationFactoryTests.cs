ca8a058852610f5852b7e24ba6315845436104ff
eglauko <eglauko@hotmail.com>
2022-03-07 21:51:28 -0300

    Tests 02

--- Diagnostics/RoyalCode.Diagnostics.Tests/T02_DiagnosticsOperationFactoryTests.cs
+++ Diagnostics/RoyalCode.Diagnostics.Tests/T02_DiagnosticsOperationFactoryTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Xunit;
 
 namespace RoyalCode.Diagnostics.Tests;
@@ -10,16 +11,95 @@ public class T02_DiagnosticsOperationFactoryTests
         Assert.Equal("RoyalCode.Diagnostics.Tests", TestsDiagnostic.ListenerName);
         Assert.Equal("RoyalCode.Diagnostics.Other", OtherTestsDiagnostic.ListenerName);
     }
+
+    [Fact]
+    public void T02_SendEvent()
+    {
+        List<Tuple<string, object?>> items = new();
+        object testData = new object();
+
+        void callback(string eventName, object? eventData) =>
+            items.Add(new Tuple<string, object?>(eventName, eventData));
+
+        DiagnosticListener.AllListeners.Subscribe(new TestListnerObserver(callback));
+
+        var operation = TestsDiagnostic.CreateTestAlphaOperation();
+        
+        operation.AddItem(testData);
+        operation.FireEvent("TestAlpha");
+        
+        Assert.Single(items);
+        Assert.Same(operation, items[0].Item2);
+    }
+    
+    [Fact]
+    public void T03_Event_Start_Stop()
+    {
+        List<Tuple<string, object?>> items = new();
+
+        void callback(string eventName, object? eventData) =>
+            items.Add(new Tuple<string, object?>(eventName, eventData));
+
+        DiagnosticListener.AllListeners.Subscribe(new TestListnerObserver(callback));
+
+        var operation = TestsDiagnostic.CreateTestAlphaOperation();
+        
+        operation.Start();
+        operation.Dispose();
+        
+        Assert.Equal(2, items.Count);
+        Assert.EndsWith(".Start", items[0].Item1);
+        Assert.EndsWith(".Stop", items[1].Item1);
+    }
 }
 
 [DiagnosticListenerName("RoyalCode.Diagnostics.Tests")]
 public class TestsDiagnostic : DiagnosticOperationFactory<TestsDiagnostic>
 {
-    
+    public static DiagnosticOperation CreateTestAlphaOperation()
+        => CreateOperation("TestAlpha");
 }
 
 [DiagnosticListenerName("RoyalCode.Diagnostics.Other")]
 public class OtherTestsDiagnostic : DiagnosticOperationFactory<OtherTestsDiagnostic>
 {
     
+}
+
+public class TestListnerObserver : IObserver<DiagnosticListener>
+{
+    private readonly Action<string, object?> callback;
+
+    public TestListnerObserver(Action<string, object?> callback)
+    {
+        this.callback = callback;
+    }
+
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(DiagnosticListener value)
+    {
+        value.Subscribe(new TestEventObserver(callback));
+    }
+}
+
+public class TestEventObserver : IObserver<KeyValuePair<string, object?>>
+{
+    private readonly Action<string, object?> callback;
+
+    public TestEventObserver(Action<string, object?> callback)
+    {
+        this.callback = callback;
+    }
+
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(KeyValuePair<string, object?> value)
+    {
+        callback(value.Key, value.Value);
+    }
 }
