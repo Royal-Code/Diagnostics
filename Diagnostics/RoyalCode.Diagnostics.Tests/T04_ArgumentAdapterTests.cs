14eac8b0e721b22d693899025b17b1c11c3b5147
eglauko <eglauko@hotmail.com>
2022-03-18 18:09:39 -0300

    starting create new tests

--- Diagnostics/RoyalCode.Diagnostics.Tests/T04_ArgumentAdapterTests.cs
+++ Diagnostics/RoyalCode.Diagnostics.Tests/T04_ArgumentAdapterTests.cs
@@ -5,5 +5,25 @@ namespace RoyalCode.Diagnostics.Tests;
 
 public class T04_ArgumentAdapterTests
 {
+
+    public void T01_()
+    {
+        var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
+        
+        //diagnostic.Write(nameof(T01_), new ArgumentAdapterEventFoo() );
+        
+        
+
+
+    }
+}
+
+public class ArgumentAdapterEventFoo
+{
+    
+}
+
+public class ArgumentAdapterEventBar
+{
     
 }
