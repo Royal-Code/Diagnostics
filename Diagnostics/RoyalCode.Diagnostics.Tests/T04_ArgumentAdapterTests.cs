883e644d29ff5349e0b9307910b244fb4cff9145
eglauko <eglauko@hotmail.com>
2022-03-19 22:30:10 -0300

    tests completed.

--- Diagnostics/RoyalCode.Diagnostics.Tests/T04_ArgumentAdapterTests.cs
+++ Diagnostics/RoyalCode.Diagnostics.Tests/T04_ArgumentAdapterTests.cs
@@ -5,25 +5,195 @@ namespace RoyalCode.Diagnostics.Tests;
 
 public class T04_ArgumentAdapterTests
 {
-
-    public void T01_()
+    [Fact]
+    public void T01_SimpleObserverEvent()
     {
+        object? arg1 = null;
+        void Callback(object a1) => arg1 = a1;
+        var observer = DiagnosticListenerObserver.Create(nameof(T04_ArgumentAdapterTests), addHandlers =>
+        {
+            addHandlers(DiagnosticEventHandlers.For<object>(nameof(T01_SimpleObserverEvent), Callback));
+        });
+        DiagnosticListener.AllListeners.Subscribe(observer);
+
         var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
-        
-        //diagnostic.Write(nameof(T01_), new ArgumentAdapterEventFoo() );
-        
-        
+        diagnostic.Write(nameof(T01_SimpleObserverEvent), new ArgumentAdapterEventFoo("Test", "01", ArgumentAdapterEventFooEnum.A));
 
+        Assert.NotNull(arg1);
+        Assert.Equal(typeof(ArgumentAdapterEventFoo), arg1!.GetType());
+    }
 
+    [Fact]
+    public void T02_ObserverPropertyNames()
+    {
+        string? argName = null;
+        string? argValue = null;
+        void Callback(string name, string value)
+        {
+            argName = name;
+            argValue = value;
+        };
+
+        var observer = DiagnosticListenerObserver.Create(nameof(T04_ArgumentAdapterTests), addHandlers =>
+        {
+            addHandlers(DiagnosticEventHandlers.For<string, string>(nameof(T02_ObserverPropertyNames), Callback, "Name", "Value"));
+        });
+        DiagnosticListener.AllListeners.Subscribe(observer);
+
+        var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
+        diagnostic.Write(nameof(T02_ObserverPropertyNames), new ArgumentAdapterEventFoo("Test", "02", ArgumentAdapterEventFooEnum.A));
+
+        Assert.Equal("Test", argName);
+        Assert.Equal("02", argValue);
+    }
+
+    [Fact]
+    public void T03_SimpleArgumentAdapter()
+    {
+        ArgumentAdapterSingleObserverAlpha? arg1 = null;
+        void Callback(ArgumentAdapterSingleObserverAlpha a1) => arg1 = a1;
+        var observer = DiagnosticListenerObserver.Create(nameof(T04_ArgumentAdapterTests), addHandlers =>
+        {
+            addHandlers(DiagnosticEventHandlers.For<ArgumentAdapterSingleObserverAlpha>(nameof(T03_SimpleArgumentAdapter), Callback));
+        });
+        DiagnosticListener.AllListeners.Subscribe(observer);
+
+        var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
+        diagnostic.Write(nameof(T03_SimpleArgumentAdapter), new ArgumentAdapterEventFoo("Test", "03", ArgumentAdapterEventFooEnum.A));
+
+        Assert.NotNull(arg1);
+        Assert.Equal("Test", arg1!.Name);
+        Assert.Equal("03", arg1!.Value);
+    }
+
+    [Fact]
+    public void T04_ArgumentAdapterWithPropertyNames()
+    {
+        ArgumentAdapterSingleObserverBetha? arg1 = null;
+        void Callback(ArgumentAdapterSingleObserverBetha a1) => arg1 = a1;
+        var observer = DiagnosticListenerObserver.Create(nameof(T04_ArgumentAdapterTests), addHandlers =>
+        {
+            addHandlers(DiagnosticEventHandlers.For<ArgumentAdapterSingleObserverBetha>(nameof(T04_ArgumentAdapterWithPropertyNames), Callback));
+        });
+        DiagnosticListener.AllListeners.Subscribe(observer);
+
+        var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
+        diagnostic.Write(nameof(T04_ArgumentAdapterWithPropertyNames), new ArgumentAdapterEventFoo("Test", "04", ArgumentAdapterEventFooEnum.A));
+
+        Assert.NotNull(arg1);
+        Assert.Equal("Test", arg1!.BethaName);
+        Assert.Equal("04", arg1!.BethaValue);
+    }
+
+    [Fact]
+    public void T05_ArgumentAdapterMultiTypes()
+    {
+        ArgumentAdapterSingleObserverGamma? arg1 = null;
+        void Callback(ArgumentAdapterSingleObserverGamma a1) => arg1 = a1;
+        var observer = DiagnosticListenerObserver.Create(nameof(T04_ArgumentAdapterTests), addHandlers =>
+        {
+            addHandlers(DiagnosticEventHandlers.For<ArgumentAdapterSingleObserverGamma>(nameof(T05_ArgumentAdapterMultiTypes), Callback));
+        });
+        DiagnosticListener.AllListeners.Subscribe(observer);
+
+        var diagnostic = new DiagnosticListener(nameof(T04_ArgumentAdapterTests));
+        diagnostic.Write(nameof(T05_ArgumentAdapterMultiTypes), new ArgumentAdapterEventFoo("TestFoo", "05Foo", ArgumentAdapterEventFooEnum.A));
+
+        Assert.NotNull(arg1);
+        Assert.Equal("TestFoo", arg1!.Name);
+        Assert.Equal("05Foo", arg1!.Value);
+        Assert.Null(arg1.Description);
+        Assert.Null(arg1.Information);
+
+        arg1 = null;
+        diagnostic.Write(nameof(T05_ArgumentAdapterMultiTypes), new ArgumentAdapterEventBar("TestBar", "05Bar", ArgumentAdapterEventBarEnum.A));
+
+        Assert.NotNull(arg1);
+        Assert.Equal("TestBar", arg1!.Description);
+        Assert.Equal("05Bar", arg1!.Information);
+        Assert.Null(arg1.Name);
+        Assert.Null(arg1.Value);
     }
 }
 
 public class ArgumentAdapterEventFoo
 {
-    
+    public ArgumentAdapterEventFoo(string name, string value, ArgumentAdapterEventFooEnum type)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Value = value ?? throw new ArgumentNullException(nameof(value));
+        Type = type;
+    }
+
+    public string Name { get; }
+
+    public string Value { get; }
+
+    public ArgumentAdapterEventFooEnum Type { get; }
+}
+
+public enum ArgumentAdapterEventFooEnum
+{
+    A, B, C
 }
 
 public class ArgumentAdapterEventBar
 {
-    
+    public ArgumentAdapterEventBar(string description, string information, ArgumentAdapterEventBarEnum type)
+    {
+        Description = description ?? throw new ArgumentNullException(nameof(description));
+        Information = information ?? throw new ArgumentNullException(nameof(information));
+        Type = type;
+    }
+
+    public string Description { get; }
+
+    public string Information { get; }
+
+    public ArgumentAdapterEventBarEnum Type { get; }
+}
+
+public enum ArgumentAdapterEventBarEnum
+{
+    A, B, C
+}
+
+[ArgumentAdapter]
+public class ArgumentAdapterSingleObserverAlpha
+{
+    [GetFromArgument]
+    public string? Name { get; set; }
+
+    [GetFromArgument]
+    public string? Value { get; set; }
+}
+
+[ArgumentAdapter]
+public class ArgumentAdapterSingleObserverBetha
+{
+    [GetFromArgument("Name")]
+    public string? BethaName { get; set; }
+
+    [GetFromArgument("Value")]
+    public string? BethaValue { get; set; }
+}
+
+
+[ArgumentAdapter(GetFromTypes = new [] {
+    "RoyalCode.Diagnostics.Tests.ArgumentAdapterEventFoo, RoyalCode.Diagnostics.Tests",
+    "RoyalCode.Diagnostics.Tests.ArgumentAdapterEventBar, RoyalCode.Diagnostics.Tests"
+})]
+public class ArgumentAdapterSingleObserverGamma
+{
+    [GetFromArgument(Required = false)]
+    public string? Name { get; set; }
+
+    [GetFromArgument(Required = false)]
+    public string? Value { get; set; }
+
+    [GetFromArgument(Required = false)]
+    public string? Description { get; set; }
+
+    [GetFromArgument(Required = false)]
+    public string? Information { get; set; }
 }
---
+++ Diagnostics/RoyalCode.Diagnostics.Tests/T05_AspNetCoreTests.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace RoyalCode.Diagnostics.Tests;
+
+public class T05_AspNetCoreTests
+{
+    [Fact]
+    public async Task T01_ObserveFromServiceForWorker()
+    {
+        var app = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddDiagnosticListenerObserver().AddObserver<AspNetCoreDiagnosticEventObserver>();
+            }).Build();
+
+        app.SubscribeDiagnosticListenerObserver();
+
+        var startTask = app.RunAsync();
+
+        var observer = app.Services.GetService<IDiagnosticEventObserver>() as AspNetCoreDiagnosticEventObserver;
+
+        Assert.NotNull(observer);
+        Assert.Empty(observer!.Operations);
+
+        var operation = TestsDiagnostic.CreateTestAlphaOperation();
+        operation.FireEvent(nameof(T01_ObserveFromServiceForWorker));
+
+        Assert.Single(observer!.Operations);
+
+        await app.StopAsync(new CancellationToken(true));
+    }
+
+    [Fact]
+    public async Task T02_ObserveFromServiceForWeb()
+    {
+
+        var app = WebHost.CreateDefaultBuilder<TestStartup>(new string[0])
+            .ConfigureServices(services =>
+            {
+                services.AddDiagnosticListenerObserver().AddObserver<AspNetCoreDiagnosticEventObserver>();
+            })
+            .Build();
+
+        WebHost.CreateDefaultBuilder();
+
+        app.Start();
+
+        var observer = app.Services.GetService<IDiagnosticEventObserver>() as AspNetCoreDiagnosticEventObserver;
+
+        Assert.NotNull(observer);
+        Assert.Empty(observer!.Operations);
+
+        var operation = TestsDiagnostic.CreateTestAlphaOperation();
+        operation.FireEvent(nameof(T02_ObserveFromServiceForWeb));
+
+        Assert.Single(observer!.Operations);
+
+        await app.StopAsync(new CancellationToken(true));
+    }
+}
+
+public class AspNetCoreDiagnosticEventObserver : DiagnosticEventObserverBase
+{
+    public ICollection<DiagnosticOperation> Operations { get; } = new List<DiagnosticOperation>();
+
+    public override string DiagnosticListenerName => "RoyalCode.Diagnostics.Tests";
+
+    protected override IEnumerable<IDiagnosticEventHandler> CreateHandlers()
+    {
+        yield return DiagnosticEventHandlers.For<DiagnosticOperation>(
+                nameof(T05_AspNetCoreTests.T01_ObserveFromServiceForWorker),
+                EventObserverHandler);
+
+        yield return DiagnosticEventHandlers.For<DiagnosticOperation>(
+                nameof(T05_AspNetCoreTests.T02_ObserveFromServiceForWeb),
+                EventObserverHandler);
+    }
+
+    private void EventObserverHandler(DiagnosticOperation obj)
+    {
+        Operations.Add(obj);
+    }
+}
+
+public class TestStartup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+    }
+}
